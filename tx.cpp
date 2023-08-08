4ccfe16afdb2f447487fae6a9a3eb8de1f11bea7
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-08-08 23:12:13 +0530

    Add files via upload

---
+++ tx.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void rotateLeft(int arr[], int size, int rotations) {
+    rotations %= size; // To handle cases where rotations > size
+
+    for (int i = 0; i < rotations; ++i) {
+        int temp = arr[0];
+        for (int j = 0; j < size - 1; ++j) {
+            arr[j] = arr[j + 1];
+        }
+        arr[size - 1] = temp;
+    }
+}
+
+int main() {
+    int size, rotations;
+
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter the elements of the array:\n";
+    for (int i = 0; i < size; ++i) {
+       cin >> arr[i];
+    }
+
+    cout << "Enter the number of rotations: ";
+    cin >> rotations;
+
+    rotateLeft(arr, size, rotations);
+
+    cout << "Array after " << rotations << " left rotations:\n";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
