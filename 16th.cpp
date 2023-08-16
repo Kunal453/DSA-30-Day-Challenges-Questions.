bc427338fb2e5ec6a352fcddc5359cd8ba0c9df5
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-08-17 00:49:57 +0530

    Update 16th.cpp

--- 16th.cpp
+++ 16th.cpp
@@ -1,1 +1,26 @@
+// Find the GCD (Greatest Common Divisor) of 2 integers. The GCD of any 2 numbers is the largest number that divides both the integers?
+
+#include <iostream>
+using namespace std;
+
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    int result = gcd(num1, num2);
+    cout << "GCD of " << num1 << " and " << num2 << " is " << result << endl;
+
+    return 0;
+}
 
