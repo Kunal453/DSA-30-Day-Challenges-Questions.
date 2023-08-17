7fe4b01fd97caf3bf9a132dc10989e5edba1e38e
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-08-17 13:30:44 +0530

    Create 17th.cpp

---
+++ 17th.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+bool isFibonacci(int n)
+{
+
+    if (n == 0 || n == 1)
+    {
+        return true;
+    }
+
+    int a = 0;
+    int b = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+
+        int c = a + b;
+
+        if (c == n)
+        {
+            return true;
+        }
+
+        a = b;
+        b = c;
+    }
+
+    return false;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // Check if the number is a member of the Fibonacci sequence.
+    bool isMember = isFibonacci(n);
+
+    // Print the result.
+    if (isMember)
+    {
+        cout << "The number " << n << " is a member of the Fibonacci sequence." << endl;
+    }
+    else
+    {
+        cout << "The number " << n << " is not a member of the Fibonacci sequence." << endl;
+    }
+
+    return 0;
+}
