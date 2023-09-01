e6a42ce8d6ff244229d30fee1396054bf1b54e17
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-09-01 23:28:43 +0530

    Update 28th.cpp

--- 28th.cpp
+++ 28th.cpp
@@ -1,56 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-
 using namespace std;
 
-class Graph {
-private:
-    int V; // Number of vertices
-    vector<vector<int>> adj; // Adjacency list
+bool isBipartite(vector<vector<int>> adj, int n) {
+  vector<int> color(n, -1);
+  bool bipartite = true;
 
-public:
-    Graph(int vertices) {
-        V = vertices;
-        adj.resize(V);
-    }
+  for (int i = 0; i < n; i++) {
+    if (color[i] == -1) {
+      color[i] = 0;
+      queue<int> q;
+      q.push(i);
 
-    // Function to add an edge to the graph
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u); // For an undirected graph
-    }
+      while (!q.empty()) {
+        int u = q.front();
+        q.pop();
 
-    // Function to check if the graph is bipartite
-    bool isBipartite() {
-        vector<int> color(V, -1); // Initialize colors to -1 (unassigned)
-        
-        for (int start = 0; start < V; ++start) {
-            if (color[start] == -1) { // If the current component is unvisited
-                queue<int> q;
-                q.push(start);
-                color[start] = 0; // Assign the color 0 to the starting node
-
-                while (!q.empty()) {
-                    int u = q.front();
-                    q.pop();
-
-                    for (int v : adj[u]) {
-                        if (color[v] == -1) {
-                            color[v] = 1 - color[u]; // Assign the opposite color
-                            q.push(v);
-                        } else if (color[v] == color[u]) {
-                            return false; // The graph is not bipartite
-                        }
-                    }
-                }
-            }
+        for (int v : adj[u]) {
+          if (color[v] == -1) {
+            color[v] = (color[u] + 1) % 2;
+            q.push(v);
+          } else if (color[v] == color[u]) {
+            bipartite = false;
+            break;
+          }
         }
-        
-        return true; // If no conflict was found, the graph is bipartite
+
+        if (!bipartite) break;
+      }
     }
-};
+  }
+
+  return bipartite;
+}
 
 int main() {
-    // Create a graph with 4 vertices
-   
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> adj(n);
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  bool bipartite = isBipartite(adj, n);
+  if (bipartite) cout << "Yes" << endl;
+  else cout << "No" << endl;
+
+  return 0;
+}
